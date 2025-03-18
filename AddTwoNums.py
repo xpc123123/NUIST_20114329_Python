7b04f0044f4ca1ba62c2dd3c5e699d1e9944f2d5
xpc123123 <1320401599@qq.com>
2025-03-18 19:30:59 +0800

    Version 2

--- AddTwoNums.py
+++ AddTwoNums.py
@@ -1,8 +1,9 @@
 # Add Two Numbers in Python
-# Author: Your Name here
-# Using the + Operator
-a = 15
-b = 12
-# Adding two numbers
-res = a + b
+# Author: xiepengcheng
+# Using user input
+# taking user input
+a = input("First number: ")
+b = input("Second number: ")
+# converting input to float and adding
+res = float(a) + float(b)
 print(res)
